2c82cda10fd201c650e8978d86016c97f6d67bf0
prabhas1401 <prabhas29nov@gmail.com>
2025-08-12 12:55:30 +0530

    Added Spring Boot backend project

---
+++ BackendChillBilling/src/main/java/com/chillbilling/BackendChillBillingApplication.java
@@ -0,0 +1,13 @@
+package com.chillbilling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendChillBillingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendChillBillingApplication.class, args);
+	}
+
+}
